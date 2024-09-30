fa58b3d27dca2fd5cfde50541b97581a1b01bf1c
Hunor Márton Borbély <bor.hunor@gmail.com>
2024-09-30 22:31:49 +0200

    Add Sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,8 @@ import mdx from "@astrojs/mdx";
 
 import react from "@astrojs/react";
 
+import sentry from "@sentry/astro";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
@@ -16,7 +18,17 @@ export default defineConfig({
   adapter: vercel({
     imageService: true,
   }),
-  integrations: [mdx(), react()],
+  integrations: [
+    mdx(),
+    react(),
+    sentry({
+      dsn: "https://c2e552f56cd9a0c4c68bfe50cc0c424b@o4506893552975872.ingest.us.sentry.io/4508043663507456",
+      sourceMapsUploadOptions: {
+        project: "svg-tutorial",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
   redirects: {
     "/arc": "/editor/arc",
     "/cubicBezier": "/editor/cubicBezier",
