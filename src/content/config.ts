14392f9070416ebce3eaaf9f90efa36bd799bb17
Hunor Márton Borbély <bor.hunor@gmail.com>
2024-11-26 00:38:53 +0100

    Group lessons into categories
    Show the category in the breadcrumb with link to the table of content and group table of content into category groups as well

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,19 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 // 2. Define your collection(s)
 const svgCollection = defineCollection({
-    type: 'content', // v2.5.0 and later
-    schema: () => z.object({
-        day: z.number(),
-        title: z.string(),
-        component: z.string(),
-        description: z.string(),
+  type: "content", // v2.5.0 and later
+  schema: () =>
+    z.object({
+      day: z.number(),
+      title: z.string(),
+      component: z.string(),
+      category: z.string(),
+      description: z.string(),
     }),
 });
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-    'svg': svgCollection,
+  svg: svgCollection,
 };
