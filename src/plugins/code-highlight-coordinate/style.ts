22882a34a9316de9755b0fd4bb803eb9ec1cb9ff
Hunor Márton Borbély <bor.hunor@gmail.com>
2024-12-01 15:02:01 +0100

    For path elements only highlight one coordinate at a time on hover

--- src/plugins/code-highlight-coordinate/style.ts
+++ src/plugins/code-highlight-coordinate/style.ts
@@ -1,7 +1,7 @@
 export default `
 
 .coordinate:hover {
-    background-color: rgba(38, 74, 137, 0.8);
+    background-color: rgba(57, 100, 173, 0.8); /* Matches the highlight-coordinate.scss utility */
 }
 
 `;
